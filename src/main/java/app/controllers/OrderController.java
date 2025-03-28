1b77904f5dba18137c15715834b04bbc1edfa85b
miekgram <cph-mg463@cphbusiness.dk>
2025-03-28 21:19:04 +0100

    Update OrderController.java

--- src/main/java/app/controllers/OrderController.java
+++ src/main/java/app/controllers/OrderController.java
@@ -7,6 +7,7 @@ import app.persistence.CupcakeMapper;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
 
+import java.io.IOException;
 import java.util.List;
 
 public class OrderController {
@@ -21,16 +22,21 @@ public class OrderController {
     private static void addOrder(Context ctx, ConnectionPool connectionPool) throws DatabaseException {
     //skal tilføje en ordre
         // vi skal have bund, topping og amount som parametre
-        //skal proppes i en form for liste så de gemmes
-        // toppings = CupcakeMapper.getAllToppings(connectionPool);
+            //skal proppes i en form for liste så de gemmes
+            // toppings = CupcakeMapper.getAllToppings(connectionPool);
 
-        //TODO lav HTML FORM
-            //Der gemmer dropdown svar
-            //Når der trykkes på knap (tilføj kurv)
-            //Så skal der sendes en post, der rendere til en action som peger på denne function addOrder.
-        String bottom = ctx.formParam("bottom");
-        String topping = ctx.formParam("topping");
-        String amount = ctx.formParam("amount");
+            //TODO lav HTML FORM
+                //Der gemmer dropdown svar
+                //Når der trykkes på knap (tilføj kurv)
+                //Så skal der sendes en post, der rendere til en action som peger på denne function addOrder.
+            String bottom = ctx.formParam("bottom");
+            String topping = ctx.formParam("topping");
+            String amount = ctx.formParam("amount");
+
+            if (bottom == null || topping == null || amount == null) {
+                ctx.status(400).result("Missing required fields.");
+                return;
+            }
 
 
     }
